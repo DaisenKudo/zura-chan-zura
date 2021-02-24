54783a2a8ee378070b5e7b6e36bd3f9f037d67fb
DaisenK <b1017150@fun.ac.jp>
2021-02-24 17:18:45 +0900

    掃除

--- main.go
+++ main.go
@@ -5,17 +5,4 @@ import "Zura-chanZura/app/infrastructure"
 func main() {
 	r := infrastructure.NewRouting()
 	r.Run()
-
-	/*engine := gin.Default()
-
-	engine.Use(favicon.New("./assets/icon/favicon.ico"))
-	engine.LoadHTMLGlob("templates/*")
-	engine.Static("/assets", "./assets")
-	engine.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title" : "ずらちゃんずら💓",
-			"text" : "ずらちゃんずら💓",
-		})
-	})
-	engine.Run(":8080")*/
 }
