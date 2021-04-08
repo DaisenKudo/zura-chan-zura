4b4daa5b0915e5cabf68c6b453069ce13f73dd3d
DaisenK <b1017150@fun.ac.jp>
2021-04-09 00:55:40 +0900

    chg color

--- service/ts/bundle.ts
+++ service/ts/bundle.ts
@@ -2,6 +2,6 @@ const CLASSNAME = "visible";
 const WAIT = 0;
 
 setInterval(() => {
-    $(".bg1").addClass(CLASSNAME);
-    $(".bg2").addClass(CLASSNAME);
+    $(".bg-1").addClass(CLASSNAME);
+    $(".bg-2").addClass(CLASSNAME);
 }, WAIT);
