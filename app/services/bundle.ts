6332ad5eb350cf2297f9276c8d588727a9c053b7
DaisenK <b1017150@fun.ac.jp>
2021-04-15 21:59:46 +0900

    [mod]animation

--- app/services/bundle.ts
+++ app/services/bundle.ts
@@ -2,6 +2,7 @@ import "bootstrap-honoka"
 import "./style.scss"
 
 (function () {
+    const SPEED = 0.5;
 
      let unit = 100,
          canvas: HTMLCanvasElement,
@@ -35,9 +36,9 @@ import "./style.scss"
         context.clearRect(0, 0, width, height);
 
         //波を描画
-        drawWave('#10c2cd', 0.3, 3, 0);
-        drawWave('#43c0e4', 0.4, 2, 250);
-        drawWave('#1d82b6', 0.2, 1.6, 100);
+        drawWave('#a0c0d4', 0.5, 3, SPEED,0);
+        drawWave('#7ba4d9', 0.6, 2, SPEED * 1.5, 87);
+        drawWave('#c0faff', 0.4, 1.6, SPEED * 2.0, 174);
 
         // Update the time and draw again
         draw.seconds = draw.seconds + 0.014;
@@ -47,19 +48,19 @@ import "./style.scss"
     draw.seconds = 0;
     draw.t = 0;
 
-    function drawWave(color: string, alpha: number, zoom: number, delay: number) {
+    function drawWave(color: string, alpha: number, zoom: number, speed: number, delay: number) {
         context.fillStyle = color;
         context.globalAlpha = alpha;
 
         context.beginPath(); //パスの開始
-        drawSine(draw.t / 0.5, zoom, delay);
+        drawSine(draw.t / 0.5, zoom, speed, delay);
         context.lineTo(width + 10, height); //パスをCanvasの右下へ
         context.lineTo(0, height); //パスをCanvasの左下へ
         context.closePath() //パスを閉じる
         context.fill(); //塗りつぶす
     }
 
-    function drawSine(t: number, zoom: number, delay: number) {
+    function drawSine(t: number, zoom: number, speed: number, delay: number) {
 
         // Set the initial x and y, starting at 0,0 and translating to the origin on
         // the canvas.
@@ -69,7 +70,7 @@ import "./style.scss"
 
         // Loop to draw segments (横幅の分、波を描画)
         for (let i = yAxis; i <= width + 10; i += 10) {
-            x = t + (-yAxis + i) / unit / zoom;
+            x = (t + (-yAxis + i) / unit / zoom) * speed;
             y = Math.sin(x - delay);
             context.lineTo(i, unit * y + xAxis);
         }
