a81b343873b074970dc95c722c82bedc5ceed205
Squatarola <b1017150@fun.ac.jp>
2022-03-26 22:56:43 +0900

    Dev (#63)
    * Fix for CVE-2020-28483
    
    * Fix for CVE-2020-28483
    
    * Update main_test.go
    
    * Update main_test.go
    
    Co-authored-by: squatarola <squ4r4r014@gmail.com>

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 	"strconv"
 	"testing"
 )
@@ -12,6 +13,9 @@ const (
 
 func TestMainSuccess(t *testing.T) {
 	go main()
+	
+	time.Sleep(5000)
+	
 	result, err := http.Get("http://localhost:" + strconv.Itoa(port))
 	if err != nil {
 		t.Fatal("サーバーが起動しませんでした")
